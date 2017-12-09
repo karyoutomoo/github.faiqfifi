402ad418df638e274b12f761c43efe26aa2ed354
Faiq <karyoutomoo@gmail.com>
2017-12-09 13:44:41 +0700

    pushing employee model

---
+++ src/main/java/com/example/blibli/model/Employee.java
@@ -0,0 +1,32 @@
+package com.example.blibli.model;
+
+import java.sql.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+@Table(name = Employee.TABLE_NAME)
+public class Employee {
+	public static final String TABLE_NAME = "employee";
+	@Id
+    @Column(name="id")
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name="uuid2", strategy = "uuid2")
+    private String id;
+    
+    @Column(name="name", nullable=false)
+    private String name;
+
+    @Column(name="hiredate", nullable=false)
+    private Date hireDate;
+
+    @Column(name="status", nullable=false)
+    private String status;
+
+    }
